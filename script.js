2913cd93da6cc3206190ff32a7d000b2916b890a
Parker <pjg4086@uncw.edu>
2022-12-13 21:25:04 -0500

    new encoding

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function handleSaveForm(e) {
     let filename = document.getElementById('title').value;
     console.log(filename, data)
     let myHeaders = new Headers();
-    let formdata = new FormData();
-    formdata.append("title", filename);
-    formdata.append("content", data);
-    myHeaders.append("Content-Type", "form-data");
+    let urlencoded = new URLSearchParams();
+    urlencoded.append("title", filename);
+    urlencoded.append("content", data);
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
     let requestOptions = { 
         method: 'POST',
         headers: myHeaders,
-        body: formdata,
+        body: urlencoded,
         redirect: 'follow'
     };
 
@@ -36,8 +36,30 @@ function handleEditForm(e) {
 
 }
 
+function loadDoc(id) {
+    console.log('dnaosjndaj')
+    let joe = id
+    let myHeaders = new Headers();
+    let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+    let data = fetch(`http://127.0.0.1:3000/api/docs`)
+    data.then((response) => response.json())
+    .then((result) => {
+        console.log(result)
+        let doc = result[0]
+        console.log(doc)
+        document.getElementById('main-text').value = doc.content;
+        document.getElementById('title').value = doc.title;
+    }
+    )
+
+}
 
 saveForm.addEventListener('submit', handleSaveForm);
+
 
 
 
